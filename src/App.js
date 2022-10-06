da82f99c2ce621943854e0905bdf4c2718724d27
shota <Takashota@users.noreply.github.com>
2022-10-06 19:58:12 +0900

    New Web display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <body>
+        <section class="HeaderSection">
+          <div class="ProfileBtn">
+            <button type="button" class="btn btn-dark">Profile</button>
+          </div>
+          <div class="LinkAbout">
+            <button type="button" class="btn btn-link">About</button>
+          </div>
+          <div class="LinkBicycle">
+            <button type="button" class="btn btn-link">Bicycle</button>
+          </div>
+        </section>
+        <div class="CenterTopPicture">
+          <img src="" alt="CenterTopPicture"></img>
+        </div>
+        <div class="AboutArticle">
+         About 
+        </div>
+        <div class="BicycleArticle">
+         Bicycle 
+        </div>
+        <div class="CopyRight">
+         CopyRight 
+        </div>
+      </body> 
     </div>
   );
 }
---
+++ src/js/Profile.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './Profile.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Profile() {
+  return (
+    
+  );
+}
+
+export default Profile;
